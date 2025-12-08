c5dbc2e549a9ad4090c1875af1c84bf98f31c3e2
banyan-m <michaeltdearmas@gmail.com>
2025-12-08 18:17:58 +0000

    Update dataset.py

--- data/dataset.py
+++ data/dataset.py
@@ -9,4 +9,9 @@ os.makedirs(DATA_DIR, exist_ok=True)
 
 def list_parquet_files(data_dir=None):
     data_dir = DATA_DIR if data_dir is None else data_dir
+    parquet_files = [f for f in os.listdir(data_dir) if f.endswith(".parquet")]
+    parquet_paths = [os.path.join(data_dir, f) for f in parquet_files]
+
+    return parquet_paths
+    
 
