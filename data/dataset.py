988cf00ad35abbdc05893ff9a8657d4377c322cf
banyan-m <michaeltdearmas@gmail.com>
2025-12-08 18:36:17 +0000

    Update dataset.py

--- data/dataset.py
+++ data/dataset.py
@@ -8,10 +8,19 @@ DATA_DIR = os.path.join(base_dir, "chess_data")
 os.makedirs(DATA_DIR, exist_ok=True)
 
 def list_parquet_files(data_dir=None):
+
     data_dir = DATA_DIR if data_dir is None else data_dir
     parquet_files = [f for f in os.listdir(data_dir) if f.endswith(".parquet")]
     parquet_paths = [os.path.join(data_dir, f) for f in parquet_files]
 
     return parquet_paths
+
+def parquets_iter_batched(split, start=0, step=1, with_results=False):
     
+    assert split in ["train", "val"], "split must be either train or val"
+    parquet_paths = list_parquet_files()
+
+    
+
+
 
