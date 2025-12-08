ebb956e183a9df6e422d06f979db326e5427c4cd
banyan-m <michaeltdearmas@gmail.com>
2025-12-08 18:13:22 +0000

    Update dataset.py

--- data/dataset.py
+++ data/dataset.py
@@ -4,4 +4,9 @@ import pyarrow.parquet as pq
 from ChessTransform.common import get_base_dir
 
 base_dir = get_base_dir()
-BASE_DIR = os.path.join(base_dir, "chess_data")
+DATA_DIR = os.path.join(base_dir, "chess_data")
+os.makedirs(DATA_DIR, exist_ok=True)
+
+def list_parquet_files(data_dir=None):
+    data_dir = DATA_DIR if data_dir is None else data_dir
+
