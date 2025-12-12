741f366905e643b70fa355ef193b389b5c80b3c7
banyan-m <Michaeltdearmas@gmail.com>
2025-12-12 21:07:36 +0000

    Update tokenizer.py with BPE implementation progress

--- ChessTransform/tokenizer.py
+++ ChessTransform/tokenizer.py
@@ -1,2 +1,20 @@
+import json 
+import os
 
 
+
+class BPETokenizer:
+
+
+    def __init__(self):
+
+        self.merges = []
+
+        self.vocab = {i: bytes([i]) for i in range(256)}
+
+    @classmethod
+    def train_from_iterator(cls, iterator, vocab_size=65536):
+
+        assert vocab_size >= 256 
+
+        tokenizer = cls()
